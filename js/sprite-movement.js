62f90594336a8e86ce4e21fff27408dd40808537
Christina Edwards <cedwards239563@gmail.com>
2025-08-04 21:42:43 -0500

    added random food and enemy movement

--- js/sprite-movement.js
+++ js/sprite-movement.js
@@ -3,10 +3,13 @@ function doSpriteMovement() {
     // Food movement - moves from right to left and repeats
     if(food.x < 0) {
         food.x = 400;
+        food.y = randomNumber(0, 300);
     }
     
     // Enemy movement - moves from right to left and repeats
     if(enemy.x < 0) {
         enemy.x = 400;
+        enemy.y = randomNumber (0, 300);
     }
+
 }
