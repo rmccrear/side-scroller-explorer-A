1fe439a8ea9598d729bc06943d789096179c1b03
Alphonso Gillis <shadowtheguy912@gmail.com>
2025-08-04 21:25:25 -0500

    Enemy now rotates when in contact with player

--- js/sprite-interactions.js
+++ js/sprite-interactions.js
@@ -5,4 +5,9 @@ function doSpriteInteraction() {
         score = score + 10; // Add 10 points
         food.x = 400; // Send food back to start (right edge)
     }
+    if (player.isTouching(enemy)) {
+        enemy.rotation = 10;
+    } else {
+        enemy.rotation = 0;
+    }
 }
