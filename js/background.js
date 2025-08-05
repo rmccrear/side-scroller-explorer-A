ce55459d845d054e4b6a66a1f00fdebbe85bb423
Brittany James <livetoinspire17@gmail.com>
2025-08-04 21:25:25 -0500

    added ellipse

--- js/background.js
+++ js/background.js
@@ -1,11 +1,10 @@
-
 function drawBackground() {
-    // Blue sky background
-    background(135, 206, 235);
-    
-    // TODO: Sun on the right side
-    
-    // Grass at the bottom (100px high)
-    fill(34, 139, 34);
-    rect(0, 400 - 100, 400, 100);
+  // Blue sky background
+  background(135, 206, 235);
+
+  // TODO: Sun on the right side
+  ellipse(330, 50, 150, 150);
+  // Grass at the bottom (100px high)
+  fill(34, 139, 34);
+  rect(0, 400 - 100, 400, 100);
 }
