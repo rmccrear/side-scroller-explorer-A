5903cd200b97a363f08a4e20b97416c426e61c67
Brittany James <livetoinspire17@gmail.com>
2025-08-04 21:28:59 -0500

    added ellipse color

--- js/background.js
+++ js/background.js
@@ -3,6 +3,7 @@ function drawBackground() {
   background(135, 206, 235);
 
   // TODO: Sun on the right side
+  fill (yellow)
   ellipse(330, 50, 150, 150);
   // Grass at the bottom (100px high)
   fill(34, 139, 34);
